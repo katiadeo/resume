3a580da71edba94c20e1c6930a4492ba2ef15e80
Tatyana Mukhortova <katiadeo@Tatyanas-MacBook-Pro.local>
2022-03-14 15:25:32 +0300

    update skills section

--- src/pages/Skills/Skills.jsx
+++ src/pages/Skills/Skills.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import TypeAnimation from 'react-type-animation';
+import { skillsData } from './skillsData';
 import './Skills.scss';
 
 const Skills = () => {
@@ -9,12 +10,60 @@ const Skills = () => {
 			<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} className="titleBlock">
 				<TypeAnimation
 					cursor={false}
-					sequence={['', 1000, 'my main skills:']}
+					sequence={['', 1000, 'my main skills are:']}
 					wrapper="h3"
 					repeat={1}
 					className="title"
 				/>
 			</motion.div>
+			<div className="skillsContainer">
+				{skillsData.map((skill) => {
+					return (
+						<motion.div
+							className="skill"
+							key={skill.id}
+							initial={{ opacity: 0, y: 1000 }}
+							animate={{ opacity: 1, y: 0 }}
+							transition={{ delay: skill.delay }}
+						>
+							<div className="outer">
+								<div className="inner">
+									<img className="skillLogo" src={skill.src} alt={skill.name} />
+								</div>
+							</div>
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								version="1.1"
+								width="160px"
+								height="160px"
+								viewBox="6.5 6.5 235 235"
+							>
+								<defs>
+									<linearGradient id={skill.gradientId}>
+										<stop
+											offset="0%"
+											stopColor={skill.startColor}
+											stopOpacity="0.8"
+										/>
+										<stop
+											offset="100%"
+											stopColor={skill.stopColor}
+											stopOpacity="0.5"
+										/>
+									</linearGradient>
+								</defs>
+								<circle
+									className={skill.class}
+									cx="80"
+									cy="80"
+									r="70"
+									stroke-linecap="round"
+								/>
+							</svg>
+						</motion.div>
+					);
+				})}
+			</div>
 		</>
 	);
 };
---
+++ src/pages/Skills/skillsData.jsx
@@ -0,0 +1,69 @@
+import ReactIcon from '../../assets/png/react.png';
+import JSIcon from '../../assets/png/javascript.png';
+import HTMLIcon from '../../assets/png/html5.png';
+import CSSIcon from '../../assets/png/css-3.png';
+import SASSIcon from '../../assets/png/sass.png';
+import TSIcon from '../../assets/png/typescript.png';
+
+export const skillsData = [
+	{
+		id: 0,
+		name: 'react logo',
+		src: ReactIcon,
+		gradientId: 'GradientColorReact',
+		class: 'circleReact',
+		startColor: '#e91e63',
+		stopColor: '#673ab7',
+		delay: 0.3,
+	},
+	{
+		id: 1,
+		name: 'js logo',
+		src: JSIcon,
+		gradientId: 'GradientColorJS',
+		class: 'circleJS',
+		startColor: '#fdffa1',
+		stopColor: '#ff7300',
+		delay: 0.5,
+	},
+	{
+		id: 2,
+		name: 'html logo',
+		src: HTMLIcon,
+		gradientId: 'GradientColorHtml',
+		class: 'circleHtml',
+		startColor: '#bf2c2c',
+		stopColor: '#ff7d20',
+		delay: 0.7,
+	},
+	{
+		id: 3,
+		name: 'css logo',
+		src: CSSIcon,
+		gradientId: 'GradientColorCss',
+		class: 'circleCss',
+		startColor: '#a8ffa1',
+		stopColor: '#01309f',
+		delay: 0.9,
+	},
+	{
+		id: 4,
+		name: 'sass logo',
+		src: SASSIcon,
+		gradientId: 'GradientColorScss',
+		class: 'circleScss',
+		startColor: '#d9b4d6',
+		stopColor: '#450e6a',
+		delay: 1.1,
+	},
+	{
+		id: 5,
+		name: 'typescript logo',
+		src: TSIcon,
+		gradientId: 'GradientColorTS',
+		class: 'circleTS',
+		startColor: '#006dff',
+		stopColor: '#461564',
+		delay: 1.3,
+	},
+];
